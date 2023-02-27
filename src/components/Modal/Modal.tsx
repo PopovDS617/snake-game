0daecbffef1bccad3e048713238ba46a089bdc59
PopovDS617 <popovds.bm@gmail.com>
2023-02-27 23:58:55 +0300

    fix: bg color, animations

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -11,7 +11,7 @@ const Modal = (props: Props) => {
   const isInitial = useAppSelector((state) => state.snake.isInitial);
 
   const modalOptions = {
-    hidden: { opacity: 0, scale: 0.5 },
+    hidden: { opacity: 0, scale: 0.8 },
     enter: { opacity: 1, scale: 1 },
     exit: { opacity: 0, scale: 0.1 },
   };
